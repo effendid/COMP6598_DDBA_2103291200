f967277c84f977a5dbb22febdc7ed461b6784335
aboyphm <pebryhm@gmail.com>
2021-03-28 23:35:18 +0700

    Penambahan nomor 2

---
+++ src/TugasKelompokDuaNomorDua.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class TugasKelompokDuaNomorDua {
+  public static void main(String[] args) {
+      
+    Scanner input = new Scanner(System.in);
+	//deklarasi
+    int width, rows;
+		//input variable
+         System.out.print("Masukkan Tinggi Trapesium : ");
+         rows  = input.nextInt();
+         System.out.print("Masukkan Luas Alas Trapesium : ");
+         width  = input.nextInt();
+        
+		//kondisi
+        for (int i = rows - 1; i >= 0; --i)
+        {
+            int blocks = width - i * 2;
+            for (int j = 0; j < i; ++j)
+            {
+              System.out.print(" ");
+            }
+            for (int j = 0; j < blocks; ++j)
+            {
+			  //print
+              System.out.print("X");
+            }
+			//output
+            System.out.println();
+        }
+
+    }
+}
